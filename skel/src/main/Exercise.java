43729aa43e00b18a5af0851f6cd336c97042cd2d
Alexandru T. Zarnoianu <alex.zarnoianu@gmail.com>
2024-07-27 15:16:22 +0300

    Solution for Exercise 1

--- skel/src/main/Exercise.java
+++ skel/src/main/Exercise.java
@@ -1,11 +1,26 @@
 package main;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.util.Scanner;
-import java.io.*;
 
 public class Exercise {
-  
-  public void solve(/* TO DO: complete params list */) {
-    // TO DO
-  }
+
+    public void solve(Scanner sc, BufferedWriter bw) throws IOException {
+        String wordToChange = sc.nextLine();
+        String replacement = sc.nextLine();
+
+        while (sc.hasNext()) {
+            String newLine = sc.nextLine();
+
+            if (newLine.contains(wordToChange)) {
+                String replaced = newLine.replace(wordToChange, replacement);
+                bw.write(replaced);
+            } else bw.write(newLine);
+
+            if (sc.hasNext()) bw.write("\n");
+        }
+        bw.flush();
+    }
 }
+
