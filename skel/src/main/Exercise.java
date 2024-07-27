ee2bdd8c76ddc8c5683f9ea3fedaebe38a8f5fb4
Alexandru T. Zarnoianu <alex.zarnoianu@gmail.com>
2024-07-27 11:20:44 +0300

    Added code structure for Exercise 1

---
+++ skel/src/main/Exercise.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Exercise {
+  
+  public void solve(/* TO DO: complete params list */) {
+    // TO DO
+  }
+}
---
+++ skel/test/Checker.java
@@ -0,0 +1,54 @@
+package test;
+
+import java.io.*;
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Scanner;
+import main.Exercise;
+import org.junit.Assert;
+
+public class Checker {
+  public static long filesCompareByLine(String path1, String path2) throws IOException {
+    try (BufferedReader bf1 = new BufferedReader(new FileReader(path1));
+        BufferedReader bf2 = new BufferedReader(new FileReader(path2))) {
+
+      long lineNumber = 1;
+      String line1 = "", line2 = "";
+      while ((line1 = bf1.readLine()) != null) {
+        line2 = bf2.readLine();
+        if (line2 == null || !line1.equals(line2)) {
+          return lineNumber;
+        }
+        lineNumber++;
+      }
+      if (bf2.readLine() == null) {
+        return -1;
+      } else {
+        return lineNumber;
+      }
+    }
+  }
+
+  public static void testCase(int testNumber) {
+
+    File inputFile = new File("./test/in/test" + String.valueOf(testNumber) + ".in");
+    String outputPath = "./test/out/test" + String.valueOf(testNumber) + ".out";
+    String referencePath = "./test/ref/test" + String.valueOf(testNumber) + ".ref";
+
+    try {
+      //TO DO: Init writer
+      
+      //TO DO: Init reader
+
+      Exercise exercise = new Exercise();
+      exercise.solve(/* TO DO: fill params */);
+
+      // TO DO: close reader and writer
+
+      long differingLine = filesCompareByLine(outputPath, referencePath);
+      Assert.assertEquals(differingLine, -1);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
